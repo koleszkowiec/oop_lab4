11fb47e942ddb6f101cff07b4bd4c67a48357d8c
koleszkowiec <bartosz.swiadkowski@gmail.com>
2015-05-05 22:34:48 +0200

    beggining

---
+++ oop_lab4/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace oop_lab4
+{
+    partial class UartApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackB = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackB
+            // 
+            this.trackB.Location = new System.Drawing.Point(12, 337);
+            this.trackB.Name = "trackB";
+            this.trackB.Size = new System.Drawing.Size(206, 56);
+            this.trackB.TabIndex = 0;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(12, 275);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(206, 56);
+            this.trackBar2.TabIndex = 1;
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(12, 213);
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(206, 56);
+            this.trackBar3.TabIndex = 2;
+            // 
+            // UartApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 405);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackB);
+            this.Name = "UartApp";
+            this.Text = "RGB";
+            ((System.ComponentModel.ISupportInitialize)(this.trackB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackB;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+
+    }
+}
+
---
+++ oop_lab4/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace oop_lab4
+{
+    public partial class UartApp : Form
+    {
+        public UartApp()
+        {
+            InitializeComponent();
+            update_leds();
+        }
+
+        void update_leds()
+        {
+            rgdLedCtl1.R = rgbLedCtl2.R = (byte)trackR.Value;
+            rgdLedCtl1.G = rgbLedCtl2.G = (byte)trackG.Value;
+            rgdLedCtl1.B = rgbLedCtl2.B = (byte)trackB.Value;
+        }
+
+        private void trackR_Scroll(object sender, EventArgs e)
+        {
+            update_leds();
+        }
+
+        private void trackG_Scroll(object sender, EventArgs e)
+        {
+            update_leds();
+        }
+
+        private void trackB_Scroll(object sender, EventArgs e)
+        {
+            update_leds();
+        }
+
+
+
+        //private void Form1_Load(object sender, EventArgs e)
+        //{
+
+        //}
+    }
+
+    public partial class RgbLedCtl : UserControl
+    {
+        public enum DisplayMode
+        {
+            SeparatedColors = 1,
+            MixedColor = 2
+        }
+
+        private DisplayMode mode;
+        private byte r, g, b;
+
+        public void rgb(byte r, byte g, byte b)
+        {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+            color_changed(true);
+        }
+
+        public DisplayMode Mode
+        {
+            get {return mode;}
+            set {this.mode = value; color_changed(true);}
+        }
+
+        Brush brush, brush_r, brush_g, brush_b;
+        Pen black_pen;
+
+        public RgbLedCtl()
+        {
+            color_changed(false);
+            black_pen = new Pen(Color.FromArgb(0, 0, 0));
+            InitializeComponent();
+        }
+
+        void color_changed(bool force_invalidation)
+        {
+            brush = new SolidBrush(Color.FromArgb(r, g, b));
+            brush_r = new SolidBrush(Color.FromArgb(r, 0, 0));
+            brush_g = new SolidBrush(Color.FromArgb(0, g, 0));
+            brush_b = new SolidBrush(Color.FromArgb(0, 0, b));
+            if(force_invalidation) Invalidate();
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+            pe.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            int x = ClientRectangle.X;
+            int y = ClientRectangle.Y;
+            int w = ClientRectangle.Width;
+            int h = ClientRectangle.Height;
+            switch (mode)
+            {
+                case DisplayMode.MixedColor:
+                    pe.Graphics.FillEllipse(brush, new Rectangle(x, y, w - 1, h - 1));
+                    break;
+
+                case DisplayMode.SeparatedColors:
+
+                    break;
+
+            }
+        }
+
+        }
+
+}
